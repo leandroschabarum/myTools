b84b5f14430a6540819c2dc2cd7b68790aaeac23
Leandro Schabarum <leandroschabarum.98@gmail.com>
2021-01-31 16:55:15 -0300

    Added alert, createLOG, logRITUAL and genFILE functions

--- sentinelGoblin.sh
+++ sentinelGoblin.sh
@@ -19,11 +19,59 @@ then
 	CONF_FILE="gold.conf"  # configuration file name
 	# ----------------------------------------------------------- #
 
+	function alert() {
+		echo "$1" | "$BASE_DIR/$VENV_DIRNAME/bin/python" "$BASE_DIR/teleAlerts.py"
+		if [[ $? != 0 ]]
+		then
+			local NOW=$(date +"%d%m%Y - %H%M%S")
+			echo "$NOW />_ UNABLE TO SEND ALERT MESSAGE: $1" >> "$LOG_FILE"
+			return 1
+		else
+			return 0
+		fi
+	}
+
+	function createLOG() {
+		touch "$LOG_FILE"
+		if [[ $? != 0 ]]
+		then
+			echo "< unable to create $LOG_FILE >"
+			return 1
+		else
+			chmod 640 "$LOG_FILE"
+			chown root:root "$LOG_FILE"
+			return 0
+		fi
+	}
+
+	function logRITUAL() {
+		if [[ -f "$LOG_FILE" ]]
+		then
+			local SIZE=$(du --block=1 "$LOG_FILE" | cut -f 1)
+
+			if [[ SIZE >= 100000000 ]]
+			then
+				local COUNT=$(ls /var/log | grep "sentinelGoblin.log*" | wc -l)
+				mv "$LOG_FILE" "$LOG_FILE.$COUNT"
+				if [[ $? == 0 ]]
+				then
+					createLOG
+					return 0
+				else
+					return 1
+				fi
+			fi
+		else
+			createLOG
+			return 1
+		fi
+	}
+
 	filename_HASH=$(sha256sum "filename" | cut -d ' ' -f 1)
 
 	function checkSUM () {
 		# checkSUM filename_HASH "/path/filename" #
-		# True : changed | False : no changes #
+		# True : changed | False : no changes     #
 		local TEMP_HASH=$(sha256sum "$2" | cut -d ' ' -f 1)
 
 		if [[ "$TEMP_HASH" != "$1" ]]
@@ -35,7 +83,23 @@ then
 		fi
 	}
 
-	while true; do
+	function genFILE () {
+		# genFILE "command" "output.txt" #
+		# True : ok | False : failed     #
+		`"$1" > "$2"`
+		if [[ $? != 0 ]]
+		then
+			alert "FAILED: $1 > $2"
+			return 1
+		else
+			return 0
+		fi
+	}
+
+	while true
+	do
+		logRITUAL
 		# check routine #
+		# iptables -L > firewallRules.txt#
 	done
 fi
