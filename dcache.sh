29e2de01c7c23cbd4bb75ee32b0a62bee887f5dd
Leandro Schabarum <leandroschabarum.98@gmail.com>
2022-06-08 11:28:25 -0300

    Refactored scripts

--- dcache.sh
+++ dcache.sh
@@ -6,41 +6,51 @@
 # Contact: leandroschabarum.98@gmail.com              #
 #######################################################
 
-while getopts ":^c$:^i$:^a$" opt
+Help() {
+	cat <<- EOF >&1
+	Utility to clear memory cache.
+	$(basename "$0") [OPTIONS]
+	-p	clears only PageCache
+	-i	clears only Dentries and Inodes
+	-a	clears all PageCache, Dentries and Inodes
+	-h	displays help information
+	EOF
+}
+
+while getopts ":c:i:a:h" opt
 do
-        case $opt in
-                c)
-                        FLAG=1
-                        ;;
-                i)
-                        FLAG=2
-                        ;;
-                a)
-                        FLAG=3
-                        ;;
-                \?)
-                        echo "dcache [ -c : -i : -a]"
-                        echo "-c ....... clears only PageCache"
-                        echo "-i ....... clears only Dentries and Inodes"
-                        echo "-a ....... clears all PageCache, Dentries and Inodes"
-                        exit 1
-                        ;;
-                :)
-                        echo "requires flag option"
-                        exit 1
-                        ;;
-        esac
+	case "${opt}" in
+		c)
+			FLAG=1
+			;;
+		i)
+			FLAG=2
+			;;
+		a)
+			FLAG=3
+			;;
+		h)
+			Help
+			exit 0
+			;;
+		\?)
+			echo "Invalid option!"
+			Help
+			exit 1
+			;;
+	esac
 done
 
 if [[ $(id -u) -eq 0 ]]
 then
-        if [[ $FLAG -eq 1 || $FLAG -eq 2 || $FLAG -eq 3 ]]
-        then
-                sync
-                echo $FLAG > /proc/sys/vm/drop_caches
-                echo "done"
-        fi
+	if [[ $FLAG -eq 1 || $FLAG -eq 2 || $FLAG -eq 3 ]]
+	then
+		sync
+		echo $FLAG > /proc/sys/vm/drop_caches
+		echo "Done"
+	fi
 else
-        echo "requires sudo privileges to run"
-        exit 1
+	echo "Requires sudo privileges to run" && exit 1
 fi
+
+exit 0
