1e826a0829b63fa8e26923011abc5c27dd5d8d1d
Leandro Schabarum <leandroschabarum.98@gmail.com>
2022-08-13 00:45:04 -0300

    Completed power status monitoring script

--- power-status.sh
+++ power-status.sh
@@ -39,12 +39,12 @@ do
 done
 
 # Validate configuration file parameters
-if [[ -r "{$CFG_FILE:?'ERROR: Configuration file not set'}" ]]
+if [[ -r "${CFG_FILE:?'ERROR: Configuration file not set'}" ]]
 then
 	# Configuration file must contain:
 	#	token	- Telegram bot token
 	#	chatid	- Telegram group chat id
-	#	log		- Log file
+	#	log 	- Log file
 	#	limit	- Auto shutdown when battery is under this percentage
 	source "$CFG_FILE"
 
@@ -76,6 +76,8 @@ logger() {
 
 	MSG="${1:?'WARN: Missing message argument'}"
 
+	[[ ! -e "${log}" ]] && touch "${log}"
+
 	echo "$(date +"[%Y-%m-%d %H:%M:%S]") ${MSG}" >> "${log}" && return 0
 }
 
@@ -85,7 +87,7 @@ sendAlert() {
 
 	# expected positional argument check and message composition
 	read -r -d '' MSG <<- EOF
-	&#10071; <b>[$(hostname)]</b> <i>$(date +"%Y-%m-%d %H:%M:%S")</i>
+	<b>$(hostname)</b>  <i>@$(date +"%Y-%m-%d %H:%M:%S")</i>
 	${1:?'WARN: Missing message argument'}
 	EOF
 
@@ -103,9 +105,12 @@ sendAlert() {
 }
 
 # Evaluates control variable if it is unset
+[[ -r /tmp/pwrsply ]] && source /tmp/pwrsply
+
 if [[ -z "${PWRSPLY+x}" ]]
 then
-	export PWRSPLY=0
+	echo "PWRSPLY=0" > /tmp/pwrsply
+	[[ -r /tmp/pwrsply ]] && source /tmp/pwrsply
 fi
 
 # Evaluates current host power state
@@ -114,48 +119,52 @@ case "${STATUS:?'WARN: Status not set'}" in
 		if (( $PWRSPLY > 0 ))
 		then
 			logger "[On AC power] Battery is fully charged"
-			sendAlert "[On AC power] Battery is fully charged"
+			sendAlert "&#9889; [On AC power] Battery is fully charged"
 		fi
 
-		export PWRSPLY=0 && exit 0
+		echo "PWRSPLY=0" > /tmp/pwrsply
+		exit 0
 		;;
 	Charging)
 		logger "[On AC power] Battery is charging ( ${CHARGE:-###}% )"
 
 		if (( $PWRSPLY > 1 ))
 		then
-			sendAlert "[On AC power] Battery is charging ( ${CHARGE:-###}% )"
+			sendAlert "&#128268; [On AC power] Battery is charging ( ${CHARGE:-###}% )"
 		fi
 
-		export PWRSPLY=1 && exit 0
+		echo "PWRSPLY=1" > /tmp/pwrsply
+		exit 0
 		;;
 	Discharging)
 		logger "[On battery power] Battery is discharging ( ${CHARGE:-###}% )"
 
-		if (( ${CHARGE:-100} < $limit ))
+		if (( ${CHARGE:-100} <= $limit ))
 		then
 			logger "[Shutdown] The system will be shutdown to reserve battery for disaster prevention"
-			sendAlert "[Shutdown] The system will be shutdown to reserve battery for disaster prevention"
+			sendAlert "&#128680; [Shutdown] The system will be shutdown to reserve battery for disaster prevention"
 
 			# Machine will be shutdown after one minute
-			#shutdown -P +1
+			shutdown -P +1
 		fi
 
-		if (( $PWRSPLY > 2 ))
+		if (( $PWRSPLY < 2 ))
 		then
-			sendAlert "[On Battery power] Battery is discharging ( ${CHARGE:-###}% )"
+			sendAlert "&#128267; [On Battery power] Battery is discharging ( ${CHARGE:-###}% )"
 		fi
 
-		export PWRSPLY=2 && exit 0
+		echo "PWRSPLY=2" > /tmp/pwrsply
+		exit 0
 		;;
 	*) # DEFAULT
 		logger "[Action required] Unknown power state"
 
-		if (( $PWRSPLY > 3 ))
+		if (( $PWRSPLY != 3 ))
 		then
-			sendAlert "[Action required] Unknown power state"
+			sendAlert "&#10071; [Action required] Unknown power state"
 		fi
 
-		export PWRSPLY=3 && exit 1
+		echo "PWRSPLY=3" > /tmp/pwrsply
+		exit 1
 		;;
 esac
